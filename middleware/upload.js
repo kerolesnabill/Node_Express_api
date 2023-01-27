10dffed11247b6a5dfe7416b5c82709a58f74225
kerolesnabill <kerolesnabil513@gmail.com>
2023-01-27 09:29:51 +0200

    Add upload images for posts.

--- middleware/upload.js
+++ middleware/upload.js
@@ -11,21 +11,49 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-exports.upload = multer({
+const multerUpload = multer({
   storage,
   fileFilter,
-}).single("photo");
+});
 
-exports.resize = (req, res, next) => {
+exports.upload = multerUpload.single("photo");
+
+exports.uploadPostImages = multerUpload.fields([
+  { name: "images", maxCount: 10 },
+]);
+
+exports.resize = async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user._id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat("jpeg")
     .jpeg({ quality: 90 })
     .toFile(`uploads/users/${req.file.filename}`);
+
+  next();
+};
+
+exports.resizePostImages = async (req, res, next) => {
+  if (!req.files) return next();
+
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `post-user-${req.user._id}-${Date.now()}-${i}.jpeg`;
+
+      await sharp(req.files.images[i].buffer)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`uploads/posts/${filename}`);
+
+      i++;
+      req.body.images.push(filename);
+    })
+  );
 
   next();
 };
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const _ = require("lodash");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const { Post, validate, validateUpdate } = require("../models/Post");
+const { uploadPostImages, resizePostImages } = require("../middleware/upload");
 
 router.get("/", async (req, res) => {
   const posts = await Post.find().sort({ createdAt: -1 }).limit(15);
@@ -21,32 +22,44 @@ router.get("/me", auth, async (req, res) => {
   res.status(200).send(posts);
 });
 
-router.post("/", auth, async (req, res) => {
-  let post = { ...req.body, userId: req.user._id };
+router.post(
+  "/",
+  auth,
+  uploadPostImages,
+  resizePostImages,
+  async (req, res, next) => {
+    let post = { ...req.body, userId: req.user._id };
 
-  const { error } = validate(post);
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(post);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  post = await Post.create(post);
+    post = await Post.create(post);
 
-  res.status(200).send(post);
-});
+    res.status(200).send(post);
+  }
+);
 
-router.patch("/:id", auth, async (req, res) => {
-  let post = await Post.findById(req.params.id);
-  if (!post)
-    return res.status(404).send("There is no post with id: " + req.params.id);
+router.patch(
+  "/:id",
+  auth,
+  uploadPostImages,
+  resizePostImages,
+  async (req, res) => {
+    let post = await Post.findById(req.params.id);
+    if (!post)
+      return res.status(404).send("There is no post with id: " + req.params.id);
 
-  if (req.user._id != post.userId)
-    return res.status(400).send("You are not allowed to edit this post.");
+    if (req.user._id != post.userId)
+      return res.status(400).send("You are not allowed to edit this post.");
 
-  const { error } = validateUpdate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validateUpdate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  await post.update(req.body, { new: true });
+    await post.update(req.body, { new: true });
 
-  res.status(200).send("The post was updated successfully.");
-});
+    res.status(200).send("The post was updated successfully.");
+  }
+);
 
 router.delete("/:id", auth, async (req, res) => {
   const post = await Post.findById(req.params.id);
