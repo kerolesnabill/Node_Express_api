61b507b3b077f64b56ea3736cde31ac104faf1f8
kerolesnabill <kerolesnabil513@gmail.com>
2023-01-27 08:45:53 +0200

    Add Post route - get, post, patch, delete.

---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const _ = require("lodash");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const { Post, validate, validateUpdate } = require("../models/Post");
+
+router.get("/", async (req, res) => {
+  const posts = await Post.find().sort({ createdAt: -1 }).limit(15);
+  if (posts.length < 1) return res.status(404).send("There are no posts.");
+
+  res.status(200).send(posts);
+});
+
+// Post
+router.get("/me", auth, async (req, res) => {
+  const posts = await Post.find({ userId: req.user._id }).sort({
+    createdAt: -1,
+  });
+  if (posts.length < 1) return res.status(404).send("There are no posts.");
+
+  res.status(200).send(posts);
+});
+
+router.post("/", auth, async (req, res) => {
+  let post = { ...req.body, userId: req.user._id };
+
+  const { error } = validate(post);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  post = await Post.create(post);
+
+  res.status(200).send(post);
+});
+
+router.patch("/:id", auth, async (req, res) => {
+  let post = await Post.findById(req.params.id);
+  if (!post)
+    return res.status(404).send("There is no post with id: " + req.params.id);
+
+  if (req.user._id != post.userId)
+    return res.status(400).send("You are not allowed to edit this post.");
+
+  const { error } = validateUpdate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  await post.update(req.body, { new: true });
+
+  res.status(200).send("The post was updated successfully.");
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post)
+    return res.status(404).send("There is no post with id: " + req.params.id);
+
+  if (req.user._id != post.userId)
+    return res.status(400).send("You are not allowed to delete this post.");
+
+  post.delete();
+
+  res.status(200).send("The post was deleted successfully.");
+});
+
+module.exports = router;
--- start/routes.js
+++ start/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const posts = require("../routes/posts");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/users", users);
+  app.use("/api/posts", posts);
   app.use("/api/auth", auth);
 };
